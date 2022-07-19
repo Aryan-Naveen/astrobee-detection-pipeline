c27ce01150ff51fce47ff6e44be07014a6f8f457
Aryan-Naveen <aryan_naveen@college.harvard.edu>
2022-07-18 20:03:38 -0400

    model train script written

--- src/handrail_segmentation/worlds/plugins/DataGeneration.hh
+++ src/handrail_segmentation/worlds/plugins/DataGeneration.hh
@@ -58,7 +58,7 @@ namespace data_generation
 
     private: std::vector<ignition::math::Pose3d> handrailInspectPositions;
 
-    private: int NUM_IMAGES_EACH = 1;
+    private: int NUM_IMAGES_EACH = 200;
 
     private: int n_count = 0;
 
--- src/handrail_segmentation/worlds/segmentation_visualizer.py
+++ src/handrail_segmentation/worlds/segmentation_visualizer.py
@@ -21,12 +21,8 @@ def mouse_callback(event, x, y, flags, param):
         instance_count = labels_map[y,x,1] * 256 + labels_map[y,x,2]
         print(f'label: {label} .. instance count: {instance_count}')
 
-# Arg Parser to set the dataset path
-parser = argparse.ArgumentParser()
-parser.add_argument('--path', type=str, required=True, help='Segmentation Dataset Path')
-args = parser.parse_args()
 # dataset path
-path = args.path
+path = 'data/'
 # paths of images folders
 images_path = os.path.join(path, "images")
 labels_map_path = os.path.join(path, "labels_maps")
@@ -39,17 +35,15 @@ colored_names = sorted(os.listdir(colored_map_path))
 images_paths      = [os.path.join(images_path, name) for name in images_names]
 labels_map_paths  = [os.path.join(labels_map_path, name) for name in labels_names]
 colored_map_paths = [os.path.join(colored_map_path, name) for name in  colored_names]
+
+path_save = r'/home/anaveen/Documents/nasa_ws/astrobee-detection-pipeline/src/handrail_segmentation/worlds/data/overlayed/'
+os.mkdir(path_save)
+
 for image_path, labels_path, colored_path in zip(images_paths, labels_map_paths, colored_map_paths):
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     colored_map = cv2.imread(colored_path, cv2.IMREAD_COLOR)
     colored_image = add_colored_to_image(image, colored_map)
-    # print(cv2.imwrite("/home/anaveen/Documents/nasa_ws/astrobee-detection-pipeline/src/handrail_segmentation/worlds/data/data/segmentation/" + str(i) + ".png", colored_image))
-    imageId = os.path.splitext(image_path)[0][-7:]
-    cv2.imshow("segmentated map", colored_image)
-    k = cv2.waitKey(0)
-    while not k == 27:
-        print(k)
-        k = cv2.waitKey(0)
-    if not cv2.imwrite(r'/home/anaveen/Documents/nasa_ws/astrobee-detection-pipeline/src/handrail_segmentation/worlds/data/overlayed/segmentation_' + imageId + '.png', colored_image):
+    imageId = 'segmentation_' + os.path.splitext(image_path)[0][-7:] + '.png'
+    if not cv2.imwrite(path_save + imageId, colored_image):
         raise Exception("Could not write image")
     cv2.destroyAllWindows()
