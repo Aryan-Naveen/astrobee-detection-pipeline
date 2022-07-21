10f007a81048fa6f66ee78513364a92413f83543
Aryan-Naveen <aryan_naveen@college.harvard.edu>
2022-07-21 14:20:27 -0400

    fix

--- src/handrail_segmentation/pytorch_mrcnn/test.py
+++ src/handrail_segmentation/pytorch_mrcnn/test.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 from torchvision import transforms
 import argparse
@@ -10,6 +11,17 @@ from PIL import Image
 def get_trained_model(weights_path):
     # load an instance segmentation model pre-trained on COCO
     model = torchvision.models.detection.maskrcnn_resnet50_fpn()
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
 
     model.load_state_dict(torch.load(weights_path))
 
